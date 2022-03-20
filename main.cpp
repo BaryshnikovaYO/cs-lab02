88bf1d09d23b47f7a2d6e2d3bd578d160cc93b16
Bob (BaryshnikovaYO) <ulia29042004@yandex.ru>
2022-03-20 20:40:14 +0300

    code: печать минимума

--- main.cpp
+++ main.cpp
@@ -7,16 +7,23 @@ int main()
 cout << "Enter A and B: ";
 int a, b;
 int max;
+int min;
 cin >> a >> b;
 max = b;
 if (a > b)
     { max = a;}
 else
     { max = b;}
+min = a;
+if (b < a)
+    {min = b;}
+else
+    {min = a;}
 cout << "A + B = " << a + b << '\n'
      << "A - B = " << a - b << '\n'
      << "A * B = " << a * b << '\n'
      << "A / B = " << a / b << '\n'
-     << "Maximum = " << max << '\n';
+     << "Maximum = " << max << '\n'
+     << "Minimum = " << min << '\n';
     return 0;
 }
